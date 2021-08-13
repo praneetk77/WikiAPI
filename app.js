8f817ee455b743fcc56df8fae3ac76804a8390eb
praneetk77 <praneetkarna77@gmail.com>
2021-08-13 15:15:30 +0545

    Add get request for specific article

--- app.js
+++ app.js
@@ -28,32 +28,44 @@ const articleSchema = mongoose.Schema({
 // Creating article model
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
+/* Requests for all articles */
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) res.send("Successfully added new article.");
+      else res.send(err);
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (err) console.log(err);
+      else res.send("Successfully deleted all articles.");
+    });
+  });
+
+/* Requests for a specific article */
+app.route("/articles/:articleTitle").get(function (req, res) {
+  Article.findOne(
+    { title: req.params.articleTitle },
+    function (err, foundArticle) {
+      if (foundArticle) res.send(foundArticle);
+      else res.send("No articles matching that title was found.");
     }
-  });
-});
-
-app.post("/articles", function (req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  newArticle.save(function (err) {
-    if (!err) res.send("Successfully added new article.");
-    else res.send(err);
-  });
-});
-
-app.delete("/articles", function (req, res) {
-  Article.deleteMany(function (err) {
-    if (err) console.log(err);
-    else res.send("Successfully deleted all articles.");
-  });
+  );
 });
 
 app.listen(3000, function () {
