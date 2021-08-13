d63b2ee829c33facb6e67f3d2f12e686e4de361a
praneetk77 <praneetkarna77@gmail.com>
2021-08-13 15:57:08 +0545

    Add patch request for specific article

--- app.js
+++ app.js
@@ -78,6 +78,16 @@ app
         else res.send(err);
       }
     );
+  })
+  .patch(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function (err) {
+        if (!err) res.send("Successfully updated article.");
+        else res.send(err);
+      }
+    );
   });
 
 app.listen(3000, function () {
